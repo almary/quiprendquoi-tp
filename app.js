e72cfd01ea3fa476d484ce23420a5207223a5ef2
almary <alexandre.mary.adenni@gmail.com>
2020-03-16 17:16:40 +0100

    Etape 16

--- app.js
+++ app.js
@@ -10,28 +10,34 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "pug");
 
+
 app.get("/", function(req, res) {
   res.render("index", { title: 'Qui prend quoi ?' });
 });
 
-app.get("/party/:id", function(req, res) {
-    axios
-  .get(`${process.env.API_URL}/party/${req.params.id}`)
-  .then(({ data }) =>
-    res.render('party', {
-      party: data,
-      title: data.name
-    }),
-  )
-  .catch((err) => console.log(err));
-});
 
 app.post("/party", function(req, res) {
     axios
-    .post(`${process.env.API_URL}/party`, req.body)
-    .then(({data}) => console.log(data))
-    .catch((err) => console.error(err));
+      .post(`${process.env.API_URL}/party`, req.body)
+      .then(({ data }) => res.redirect(`/party/${data._id}`))
+      .catch((err) => res.send(err))
+      //page d'erreur à l'avenir
 });
+
+
+app.get("/party/:id", function(req, res) {
+    axios
+      .get(`${process.env.API_URL}/party/${req.params.id}`)
+      .then(({ data }) =>
+          res.render('party', {
+            party: data,
+            title: data.name,
+            url: `${process.env.FRONT_URL}:${process.env.PORT}/party/${data._id}`
+          }),
+      )
+      .catch((err) => console.log(err));
+});
+
 
 app.listen(process.env.PORT, () =>
   console.log(`Front app listening on port ${process.env.PORT}!`)
