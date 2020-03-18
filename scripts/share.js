bb937ae7e877076acfaf7fc0ff7bf12aef4f594a
almary <alexandre.mary.adenni@gmail.com>
2020-03-18 16:33:44 +0100

    étape 28

---
+++ scripts/share.js
@@ -0,0 +1,22 @@
+if (navigator.share) {
+  document.querySelectorAll("[data-share-url]").forEach($shareEl => {
+    const $button = document.createElement("button");
+    $button.innerHTML = "Partager";
+    $shareEl.parentNode.append($button);
+
+    $button.addEventListener(
+      "click",
+      shareParty.bind(this, $shareEl, $button)
+    );
+  });
+} else {
+  console.warn("Pas de support de l'api share");
+}
+
+function shareParty(element) {
+  navigator.share({
+    title: element.getAttribute("data-share-title"),
+    text: element.getAttribute("data-share-text"),
+    url: element.getAttribute("data-share-url")
+  });
+}
